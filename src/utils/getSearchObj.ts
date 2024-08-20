8fd492e38fb4803bb9dcef0cb3217624568e4062
陈云峰 <yunfeng.chen@lkcoffee.com>
2024-08-20 10:20:22 +0800

    起步 -> mock网页地址

---
+++ src/utils/getSearchObj.ts
@@ -0,0 +1,25 @@
+// src/utils/getSearchObj.ts
+// 这个函数的作用是把网页地址中的 查询参数字符串 转换为 对象
+const getSearchObj = () => {
+  // ?a=1&b=2
+  const { search } = window.location;
+
+  // a=1&b=2
+  const searchStr = search.slice(1);
+
+  // ['a=1', 'b=2']
+  const pairs = searchStr.split("&");
+
+  // { 'a': '1' }
+  const searchObj: Record<string, string> = {};
+
+  pairs.forEach((pair) => {
+    // [a, 1]
+    const [key, value] = pair.split("=");
+    searchObj[key] = value;
+  });
+
+  return searchObj;
+};
+
+export default getSearchObj;
