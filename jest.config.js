f1bff477927194d0b7a11c95d37cf07f44212776
陈云峰 <yunfeng.chen@lkcoffee.com>
2024-08-20 11:14:14 +0800

    mock timer & 快进时间

--- jest.config.js
+++ jest.config.js
@@ -85,7 +85,8 @@ module.exports = {
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   // moduleNameMapper: {},
   "moduleNameMapper": {
-    "@/(.*)": "<rootDir>/src/$1"
+    "@/(.*)": "<rootDir>/src/$1",
+    "utils/(.*)": "<rootDir>/src/utils/$1",
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
